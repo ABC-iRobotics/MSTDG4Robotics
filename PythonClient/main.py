5807ea44e10e791a6323c3f223ce63ae6b83ab7b
Pinter Adam Balazs <piadba@gmail.com>
2019-11-20 10:00:05 +0100

    PythonClient: Argumentums fixed

--- PythonClient/main.py
+++ PythonClient/main.py
@@ -7,33 +7,33 @@ import Tasks.PegTransferScene as pts
 def main():
     datasetCount = 1
     meshCount = 20
-    selectedProgram = 'BinPicking'
+    selectedTask = 'BinPicking'
     meshName = None
 
-    if selectedProgram is 'PegTransfer':
-        meshName = 'tube.obj'
     if len(sys.argv) > 1:
-        datasetCount = int(sys.argv[1])
+        selectedTask = int(sys.argv[1])
     if len(sys.argv) > 2:
-        meshCount = int(sys.argv[2])
+        datasetCount = int(sys.argv[2])
     if len(sys.argv) > 3:
-        meshName = sys.argv[3]
+        meshCount = int(sys.argv[3])
+    if len(sys.argv) > 4:
+        meshName = sys.argv[4]
 
     simulationSpeed = 1
     vrepConn = vcon.VrepConnector()
-    
+
     if vrepConn.connectionWasSuccesfull:    
         vrepConn.start()
         vrepConn.SetSimulationSpeed(simulationSpeed) #value can set from -3 (0.1x speed) to +6 (64x speed)
 
         #Logic starts here
         #-----------------
-        if selectedProgram is 'BinPicking':
+        if selectedTask is 'BinPicking':
             binPickingScene = bps.BinPickingScene(vrepConn, 'Table', 'Bin', 'Vision_sensor', meshName, meshCount)
             for dbCount in range(datasetCount):
                 binPickingScene.Step()
 
-        elif selectedProgram is 'PegTransfer':
+        elif selectedTask is 'PegTransfer':
             pegTransferScene = pts.PegTransferScene(vrepConn, meshName)
             pegTransferScene.Init('Surface', 'Vision_sensor', 'Spotlight', 'Cylinder')
             for dbCount in range(datasetCount):
